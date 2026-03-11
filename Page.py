77333ec50314c4b3323b0d7352728c36db207c44
Mathis Galloul <mathis.galloul@epitech.eu>
2026-03-11 10:03:26 +0100

    suppr page.py

--- Page.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import sys
-
-if __name__ == "__main__":
-    print("INFO: L'architecture a été modularisée.")
-    print("INFO: Redirection automatique vers app/Home.py...")
-    # On délègue l'exécution à Streamlit sur le vrai fichier principal
-    subprocess.run([sys.executable, "-m", "streamlit", "run", "app/Home.py"])
